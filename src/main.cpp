47fd85f9bad51d96a6bed5fce8552e8bf8a5b60b
Katelyn La Joie <katelynlajoie@utexas.edu>
2022-11-16 18:11:36 -0600

    Updated main.cpp to have an Address class with a distance method.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 
+#include <cmath>
+using std::sqrt;
+
+class Address {
+
+private:
+  double x, y;
+
+public:
+  
+  Address() {};
+  Address( double i, double j )
+    : x(i), y(j) {};
+
+  double distance( Address other ) {
+    double dx = x - other.x;
+    double dy = y - other.y;
+    return sqrt( dx * dx + dy * dy );
+  }
+};
+
 int main() {
+  
+  Address one(1., 1.), two(2., 2.);
+
+  std::cerr << "Distance: "
+       << one.distance(two)
+       << '\n';
+
   return 0;
+
 }
