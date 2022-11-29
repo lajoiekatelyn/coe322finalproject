27a008f3199b1f2e7897263dcc4bd453ecdf14ad
Katelyn La Joie <katelynlajoie@utexas.edu>
2022-11-29 16:25:09 -0600

    Additions made to main.cpp to include an AddressList class.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <cmath>
 using std::sqrt;
 
+#include <vector>
+using std::vector;
+
+// Address class
 class Address {
 
 private:
@@ -19,11 +23,49 @@ public:
     double dy = y - other.y;
     return sqrt( dx * dx + dy * dy );
   }
+  
+  auto getx() { return x;}
+  auto gety() { return y;}
+};
+
+// AddressList class (uses Address class)
+class AddressList {
+
+private:
+  vector<Address> list_of_addresses;
+
+public:
+  AddressList() {}
+
+  void add_address(Address address) {
+    list_of_addresses.push_back(address);
+  }
+
+   double length() {
+     double sum = 0;
+     for ( auto &address : list_of_addresses ) {
+       sum += address.distance(
+     }
+   }
+
+  // int index_closest_to() {}
+
+  void print() {
+    for( auto &element : list_of_addresses )
+      std::cout << "( " << element.getx() << " , " << element.gety() << " )" << std::endl; 
+  }
+
 };
 
 int main() {
   
   Address one(1., 1.), two(2., 2.);
+
+  AddressList stack;
+  stack.add_address(one);
+  stack.add_address(two);
+
+  stack.print();
 
   std::cerr << "Distance: "
        << one.distance(two)
