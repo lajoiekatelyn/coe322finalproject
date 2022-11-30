1d2905b582d1b8e73f4b1c043bd3eb91c2138d16
Katelyn La Joie <katelynlajoie@utexas.edu>
2022-11-30 13:06:06 -0600

    Adding modularity for Address and AddressList classes.

---
+++ src/AddressClass.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <cmath>
+using std::sqrt;
+
+// Address class
+class Address {
+
+private:
+  double x, y;
+
+public:
+
+  Address() {};
+  Address( double i, double j )
+    : x(i), y(j) {};
+
+  double distance( Address other ) {
+    double dx = x - other.x;
+    double dy = y - other.y;
+    return sqrt( dx * dx + dy * dy );
+  }
+
+  auto getx() { return x;}
+  auto gety() { return y;}
+};
---
+++ src/AddressListClass.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include <vector>
+using std::vector;
+
+#include "AddressClass.cpp"
+
+// AddressList class (uses Address class)
+class AddressList {
+
+private:
+  vector<Address> list_of_addresses;
+  Address depot(0., 0.); // remove and add to Route class.
+
+public:
+  AddressList() {}
+
+  void add_address(Address address) {
+    list_of_addresses.push_back(address);
+  }
+
+  // Assume list_of_addresses has a length greater than 0
+  double length() {
+    double sum = 0;
+    for ( int i = 1; i<list_of_addresses.size(); i++ ) {
+      Address address = list_of_addresses.at(i);
+      sum += address.distance(list_of_addresses.at(i-1));
+    }
+    return sum;
+  }
+
+  int index_closest_to(Address other) {
+    Address closest = other;
+    double closest_distance = 100000;
+    int index = 0;
+    for ( int i = 0; i<list_of_addresses.size(); i++ ) {
+      Address address = list_of_addresses.at(i);
+      double distance_between = address.distance(other);
+      if (distance_between < closest_distance) {
+        closest_distance = distance_between;
+        index = i;
+      }
+    }
+    return index;
+  }
+
+  void print() {
+    for( auto &element : list_of_addresses )
+      std::cout << "( " << element.getx() << " , " << element.gety() << " )" << std::endl;
+  }
+
+};
