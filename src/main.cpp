200f5c49862422b9d57d7f38a481133429ca50ac
Katelyn La Joie <katelynlajoie@utexas.edu>
2022-11-30 12:33:47 -0600

    Moved Address and AddressList classes to their own files for modularity.

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,31 @@
 #include <iostream>
 
+/*
 #include <cmath>
 using std::sqrt;
 
 #include <vector>
 using std::vector;
+*/
 
-// Address class
-class Address {
-
-private:
-  double x, y;
-
-public:
-  
-  Address() {};
-  Address( double i, double j )
-    : x(i), y(j) {};
-
-  double distance( Address other ) {
-    double dx = x - other.x;
-    double dy = y - other.y;
-    return sqrt( dx * dx + dy * dy );
-  }
-  
-  auto getx() { return x;}
-  auto gety() { return y;}
-};
-
-// AddressList class (uses Address class)
-class AddressList {
-
-private:
-  vector<Address> list_of_addresses;
-
-public:
-  AddressList() {}
-
-  void add_address(Address address) {
-    list_of_addresses.push_back(address);
-  }
-
-   double length() {
-     double sum = 0;
-     for ( auto &address : list_of_addresses ) {
-       sum += address.distance(
-     }
-   }
-
-  // int index_closest_to() {}
-
-  void print() {
-    for( auto &element : list_of_addresses )
-      std::cout << "( " << element.getx() << " , " << element.gety() << " )" << std::endl; 
-  }
-
-};
+//#include "AddressClass.cpp"
+#include "AddressListClass.cpp"
 
 int main() {
   
-  Address one(1., 1.), two(2., 2.);
+  Address depot(0., 0.), one(2.,0.), two(1., 0.), three(3., 0.), done(0., 0.);
 
   AddressList stack;
+  //  stack.add_address(depot);
   stack.add_address(one);
   stack.add_address(two);
+  stack.add_address(three);
+  // stack.add_address(done);
 
   stack.print();
+
+  std::cerr << stack.length() << '\n';
+  std::cerr << stack.index_closest_to(depot) << '\n';
 
   std::cerr << "Distance: "
        << one.distance(two)
